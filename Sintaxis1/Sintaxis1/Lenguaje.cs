cf7e74fea8393885bf69d3a9bf7fad6bfc9e99a4
Emmanuel Arias A <78614770+ManeAriasA@users.noreply.github.com>
2021-04-21 13:03:22 -0500

    Add files via upload

---
+++ Sintaxis1/Sintaxis1/Lenguaje.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//  Requerimiento 1: Ajustar el constructor Lexico(String) para que substraiga el nombre del archivo
+//  y el directorio.
+
+namespace Sintaxis1
+{
+    class Lenguaje: Sintaxis
+    {
+
+        public Lenguaje()
+        {
+            Console.WriteLine("Iniciando analisis gramatical.");
+        }
+
+        public Lenguaje(string nombreArch) : base(nombreArch)
+        {
+            Console.WriteLine("Iniciando analisis gramatical.");
+        }
+
+        // Programa -> Libreria Main
+        public void Program()
+        {
+            Libreria();
+            Main();
+        }
+        // Libreria -> (#include<identificador.h> Libreria) ?
+        private void Libreria()
+        {
+            if (getContenido() == "#") 
+            {   
+                match("#");
+                match("include");
+                match("<");
+                match(Token.clasificaciones.identificador);
+                match(".");
+                match("h");
+                match(">");
+
+                Libreria();
+            }
+        }
+
+        // Libreria -> #include<identificador.h> Libreria ?
+        private void Libreria2()
+        {
+            match("#");
+            match("include");
+            match("<");
+            match(Token.clasificaciones.identificador);
+            match(".");
+            match("h");
+            match(">");
+
+            if (getContenido() == "#")
+            {
+                Libreria2();
+            }
+        }
+
+        // Main -> void main(){ (Variables)? identificador := numero; }
+        private void Main()
+        {
+            match("void");
+            match("main");
+            match("(");
+            match(")");
+            match(clasificaciones.inicioBloque);
+
+            if (getClasificacion() == clasificaciones.tipoDato)
+            {
+                Variables();
+            }
+
+            match(clasificaciones.identificador);
+            match(clasificaciones.inicializacion);
+            match(clasificaciones.numero);
+            match(clasificaciones.finSentencia);
+            match(clasificaciones.finBloque);
+        }
+
+        //Lista_IDs -> identificador (,Lista_IDs)?
+        private void Lista_IDs()
+        {
+            match(clasificaciones.identificador);
+
+            if (getContenido() == ",")
+            {
+                match(",");
+                Lista_IDs();
+            }
+        }
+
+        //Variables -> tipoDato List_IDs; (Variables)?
+        private void Variables()
+        {
+            match(clasificaciones.tipoDato);
+            Lista_IDs();
+            match(clasificaciones.finSentencia);
+
+            if (getClasificacion() == clasificaciones.tipoDato)
+            {
+                Variables();
+            }
+        }
+    }
+}
---
+++ Sintaxis1/Sintaxis1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Sintaxis1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Lenguaje l = new Lenguaje("C:\\Archivos\\suma.txt"))
+                {
+                    /*while (!l.FinDeArchivo())
+                    {
+                        l.NextToken();
+                    }
+                    
+                    l.match("#");
+                    l.match("include");
+                    l.match("<");
+                    l.match(Token.clasificaciones.identificador);
+                    l.match(".");
+                    l.match("h");
+                    l.match(">"); 
+                     */
+
+                    l.Program();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
