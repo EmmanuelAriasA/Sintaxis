2d9f518b8a3780714641dc24816cbe20dff9d190
Emmanuel Arias A <78614770+ManeAriasA@users.noreply.github.com>
2021-04-23 22:18:32 -0500

    Add files via upload

---
+++ Sintaxis3/Sintaxis3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sintaxis3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Lenguaje l = new Lenguaje("C:\\Archivos\\Prueba.cpp"))
+                {
+                    /*while (!l.FinDeArchivo())
+                    {
+                        l.NextToken();
+                    }*/
+
+                    l.Program();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
