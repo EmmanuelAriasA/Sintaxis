ff96e2248762e0df3ce47db6eb251d5c0292e70f
Emmanuel Arias A <78614770+ManeAriasA@users.noreply.github.com>
2021-04-27 21:51:47 -0500

    Add files via upload

---
+++ Sintaxis3/Sintaxis3/Sintaxis.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace Sintaxis3
+{
+    class Sintaxis : Lexico
+    {
+
+        public Sintaxis()
+        {
+            Console.WriteLine("Iniciando analisis sintactico");
+            NextToken();
+        }
+
+        public Sintaxis(string nombreArch) : base(nombreArch)
+        {
+            Console.WriteLine("Iniciando analisis sintactico");
+            NextToken();
+        }
+
+        protected void match(string espera)
+        {
+
+            //Console.WriteLine(getContenido() + " = " + espera);
+            if (espera == getContenido())
+            {
+                NextToken();
+                caracter++;
+            }
+            else
+            {
+                bitacora.WriteLine("\nError de sintaxis en la linea "+ linea +", en el caracter " + caracter + ", se espera un " + espera);
+                throw new Exception("Error de sintaxis en la linea " + linea + ", en el caracter " + caracter + ", se espera un " + espera);
+            }
+        }
+
+        protected void match(clasificaciones espera)
+        {
+            //Console.WriteLine(getContenido() + " = " + espera);
+            if (espera == getClasificacion())
+            {
+                NextToken();
+                caracter++;
+            }
+            else
+            {
+                bitacora.WriteLine("\nError de sintaxis en la linea " + linea + ", en el caracter " + caracter + ", se espera un " + espera);
+                throw new Exception("Error de sintaxis en la linea "+ linea +", en el caracter " + caracter+ ", se espera un " + espera);
+            }
+        }
+
+    }
+}
